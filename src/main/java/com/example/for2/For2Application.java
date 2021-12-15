7b756a8d519f616694d398300a68d46c95542a93
rivinda.22 <QZOHIG@yandex.ru>
2021-12-15 10:48:16 +0300

    Initial commit

---
+++ src/main/java/com/example/for2/For2Application.java
@@ -0,0 +1,13 @@
+package com.example.for2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class For2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(For2Application.class, args);
+    }
+
+}
